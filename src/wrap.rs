b5e91a64e84e75839e3f085a82a3962e105dae9d
Ticki <Ticki@users.noreply.github.com>
2015-11-21 17:26:33 +0100

    Fix word wrapping

--- src/wrap.rs
+++ src/wrap.rs
@@ -4,10 +4,11 @@ pub fn wrap_msg(s: String, n: usize) -> String {
     let split = s.split(' ').collect::<Vec<_>>();
     let mut chunks = split.chunks(8).map(|x| x.join(" "));
     
-    chunks.next().unwrap_or("?".to_string()) + "\n" + &chunks.map(|ref x| {
+    chunks.next().unwrap_or("?".to_string()) + &chunks.map(|ref x| {
 
-        "           >  ".to_string() + &repeat(" ").take(n)
-                                                     .collect::<String>() + x
+        "\n           >  ".to_string() + &repeat(" ").take(n)
+                                                   .collect::<String>()
+                                     + x
 
-    }).collect::<Vec<_>>().join("\n")
+    }).collect::<String>()
 }
