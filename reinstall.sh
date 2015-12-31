3c83aed869a0165338dec688d5270e1945c3f779
Anna Ivanova <kalterfx@gmail.com>
2015-12-31 10:28:57 +0500

    Rewrite additional scripts for install.sh

--- reinstall.sh
+++ reinstall.sh
@@ -1,2 +1,20 @@
-sh uninstall.sh
-sh install.sh
+#!/bin/bash
+
+function config() {
+	source "functions.sh"
+	set -o nounset
+	return 0
+}
+
+function main() {
+	if config; then
+		if check-privileges; then
+			./uninstall.sh && ./install.sh
+		else
+			$(which sudo) ./uninstall.sh && ./install.sh
+		fi
+	fi
+}
+
+main "$@"
+
--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,22 @@
-sudo rm -rf /usr/local/lib/rustlib/dybuk
-sudo rm /usr/local/bin/dybuk
+#!/bin/bash
+
+function config() {
+	source "functions.sh"
+	set -o nounset
+	return 0
+}
+
+function main() {
+	if config; then
+		if check-privileges; then
+			rm -rvf /usr/local/bin/dybuk | perl -pe "s/^r(.+) .(.+).$/N: R\1 '\2 '\./"
+			rm -rfv /usr/local/lib/rustlib/dybuk | perl -pe "s/^r(.+) .(.+).$/N: R\1 '\2 '\./"
+		else
+			echo 'E: This script must be run with root privileges.'
+			return 1
+		fi
+	fi
+}
+
+main "$@"
+
